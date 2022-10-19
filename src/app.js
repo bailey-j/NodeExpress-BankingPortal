19eb6483b258dd0a94ec053808e70a3300abc86e
Bailey, Jozeene <jozeene.bailey@accenture.com>
2022-10-20 00:22:28 +0100

    Complete MOdule 1 - App.js

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const views = path.join(__dirname, '/views');
+
+app.set('views', views);
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Index'});
+});
+
+app.listen(3000, () => {
+    console.log('Express Server is running...');
+});
