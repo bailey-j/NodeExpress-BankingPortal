70c7822ae7d7c73f390c5f41886e35d5973e3adc
Bailey, Jozeene <jozeene.bailey@accenture.com>
2022-10-20 02:35:52 +0100

    Complete module 3

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
-const app = express();
+const app = new express();
 
 const views = path.join(__dirname, '/views');
 
@@ -17,27 +17,12 @@ const accounts = JSON.parse(accountData);
 const userData = fs.readFileSync(path.join(__dirname, '/json/users.json'),"utf8");
 const users = JSON.parse(userData);
 
-app.get('/', (req, res) => {
-    res.render('index', {title: 'Account Summary'}, {accounts: accounts});
-});
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
 
-app.get('/savings', (req, res) => {
-    res.render('account', {account: accounts.savings});
-});
+app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
+app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
+app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
 
-app.get('/checking', (req, res) => {
-    res.render('account', {account: accounts.checking});
-});
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
 
-app.get('/credit', (req, res) => {
-    res.render('account', {account: accounts.credit});
-});
-
-app.get('/profile', (req, res, done) => {
-    res.render('profile', {user: users[0]})
-    done();
-});
-
-app.listen(3000, () => {
-    console.log('Express Server is running...');
-});
+app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
