d38ef0e221772d9a65142cb4fe2345610c570a9b
Bailey, Jozeene <jozeene.bailey@accenture.com>
2022-10-21 15:03:55 +0100

    Completion of Module 5 - Using the Express Router

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const data = require('./data');
 const express = require('express');
 const app = new express();
 
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
+
 const views = path.join(__dirname, '/views');
 
 app.set('views', views);
@@ -12,32 +16,15 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded('extended', true));
+app.use(express.static(path.join(__dirname, '/public')));
 
 const { accounts, users, writeJSON } = require('./data.js');
 
 app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
-app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
-app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
-app.get('/credit', (req, res) => res.render('account', { account: accounts.credit }));
+
+app.use('/account', accountRoutes)
+app.use('/services', servicesRoutes)
 
 app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
-
-app.get('/transfer', (req, res) =>  res.render('transfer'));
-app.post('/transfer', (req, res) => { 
-    res.json('transfer', 'account["savings"].balance');
-    accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount);
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
-    writeJSON();
-    res.render('transfer', {'message': "Transfer Completed"})
-});
-
-app.get('/payment', (req, res) =>  res.render('payment', {account: accounts.credit}));
-app.post('/payment', (req, res)=> {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount);
-    
-    writeJSON();
-    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
-});
 
 app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
